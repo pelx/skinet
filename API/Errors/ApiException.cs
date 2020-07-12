af9588ecc6e31340c331c4fc428381bf20c8ccc3
pelx <laura.peppiatt@btinternet.com>
2020-07-12 09:31:11 +0100

    End of section 5

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,16 @@
+namespace API.Errors
+{
+    public class ApiException : ApiResponse
+    {
+        public ApiException(int statusCode, string message = null) : base(statusCode, message)
+        {
+        }
+
+        public ApiException(int statusCode, string details, string message = null) : base(statusCode, message)
+        {
+            Details = details;
+        }
+
+        public string Details { get; set; }
+    }
+}
