b6704460ec7ce71d825889ca6eca625ef49644b7
pelx <laura.peppiatt@btinternet.com>
2020-07-13 19:45:04 +0100

    End of Section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,9 +35,17 @@ namespace API
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
             services.AddApplicationServices();
-
             services.AddSwaggerDocumentation();
-
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("https://localhost:4200");
+                });
+            });
 
 
         }
@@ -58,6 +66,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
