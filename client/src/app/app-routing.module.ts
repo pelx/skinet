16b6906913968c7e08b90cd0ea70977151e0676b
pelx <laura.peppiatt@btinternet.com>
2020-07-21 09:56:19 +0100

    End of section 11

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
+import { ShopModule } from './shop/shop.module';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'test-error', component: TestErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  {
+    path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,12 +1,27 @@
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [NavBarComponent],
-  imports: [
-    CommonModule
+  declarations: [
+    NavBarComponent,
+    TestErrorComponent,
+    ServerErrorComponent,
+    NotFoundComponent,
   ],
-  exports: [NavBarComponent]
+  imports: [
+    CommonModule,
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+  ],
+  exports: [NavBarComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
