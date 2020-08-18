f959039505e1d50261a1f9b79fbd93a58871649c
pelx <laura.peppiatt@btinternet.com>
2020-08-18 11:32:31 +0100

    Login before text-input component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,52 @@
+import { AccountService } from './account/account.service';
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) { }
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
-    const basketId = localStorage.getItem('basket_id');
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
 
-    if (basketId) {
-      this.basketService.getBasket(basketId)
-        .subscribe(() => {
-          console.log('stored basket');
-        }, error => {
-            console.log(error);
-          });
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(
+        () => {
+          console.log('loaded user');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
   }
 
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
+
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('stored basket');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
 }
